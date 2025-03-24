94c5f7f1c9546de35fcbbe6c77b6273aef9cb6de
Jungseok Shin <jsjb7164@naver.com>
2025-03-24 21:49:54 +0900

    tailwind css 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>안녕하세요</div>;
+  return <div className=" w-full h-36 bg-gray-500">안녕하세요</div>;
 }
 
 export default App;
