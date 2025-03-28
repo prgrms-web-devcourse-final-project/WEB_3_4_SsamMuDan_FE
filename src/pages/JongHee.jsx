5d5435c799645dc264055f9dca215a4a4c44b7cf
HO-NNE <108172790+HO-NNE@users.noreply.github.com>
2025-03-28 17:38:07 +0900

    Feat #27 : select 박스

--- src/pages/JongHee.jsx
+++ src/pages/JongHee.jsx
@@ -1,2 +1,13 @@
-// 올렸다
-//sadasd
+import PrimarySelect from '@/components/common/PrimarySelect';
+
+const JongHee = () => {
+  const selectList = ['최신순', '높은 평점순'];
+  return (
+    <div className="flex justify-center mt-[100px]">
+      <div className="font-esamanru font-esamanru-bold text-[25px]">테스트</div>
+      <PrimarySelect selectList={selectList} placeholder={'최신 순'}></PrimarySelect>
+    </div>
+  );
+};
+
+export default JongHee;
