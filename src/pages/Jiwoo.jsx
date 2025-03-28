0cfdaf1bdf107b9412aeedc1bcaeed5bc1e93282
Jiwoo <erase0250@gmail.com>
2025-03-28 16:21:32 +0900

    Feat #22 : (마이페이지, 교육 배너용) 강의 카드 컴포넌트 추가

--- src/pages/Jiwoo.jsx
+++ src/pages/Jiwoo.jsx
@@ -1,6 +1,7 @@
 import Badge from '@/common/Badge';
 import LectureCard from '@/common/LectureCard';
 import StackBadge from '@/common/StackBadge';
+import LectureCardSimple from '@/components/common/LectureCardSimple';
 
 // 강의 카드 더미데이터
 const dummyLecture = {
@@ -19,7 +20,6 @@ const Jiwoo = () => {
         <StackBadge text="JavaScript" />
         <StackBadge text="TypeScript" showCloseIcon={true} />
       </div>
-
       {/* 일반 뱃지 */}
       <div className="flex gap-2">
         <Badge text="모집중" className="w-[72px] h-[33px] bg-[#3FC22E] text-white text-sm" />
@@ -27,11 +27,29 @@ const Jiwoo = () => {
         <Badge text="프론트엔드" className="w-[138px] h-[48px] bg-primary300 text-white text-2xl" />
         <Badge text="백엔드" className="w-[138px] h-[48px] border text-2xl" />
       </div>
-
       {/* 강의 카드 */}
       <div className="pl-5">
         <LectureCard {...dummyLecture} />
       </div>
+      {/* 마이페이지, 교육페이지 배너에 사용하는 강의 카드 */}
+      <div className="pl-5 flex gap-2">
+        {/* 가격 O */}
+        <LectureCardSimple
+          title={dummyLecture.title}
+          instructor={dummyLecture.instructor}
+          price={dummyLecture.price}
+          imageUrl={dummyLecture.imageUrl}
+        />
+        {/* 가격 X */}
+        <LectureCardSimple
+          title={dummyLecture.title}
+          instructor={dummyLecture.instructor}
+          price={dummyLecture.price}
+          imageUrl={dummyLecture.imageUrl}
+          showPrice={false}
+        />
+      </div>
+      <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
     </div>
   );
 };
