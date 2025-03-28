5d4d25faa8872df1cb0fd882fb3da5d885c2c50d
Jiwoo <erase0250@gmail.com>
2025-03-28 15:18:22 +0900

    Feat #16 : 강의 카드 컴포넌트 작업

--- src/common/StackBadge.jsx
+++ src/common/StackBadge.jsx
@@ -6,10 +6,9 @@ const StackBadge = ({ text, showCloseIcon = false }) => {
       <span className="text-sm leading-none">{text}</span>
       {showCloseIcon && (
         <img
-          src="/close.svg"
+          src="/icons/close.svg"
           alt="닫기 아이콘"
           className="w-2 h-2 ml-[10px] cursor-pointer inline-block align-middle"
-
         />
       )}
     </div>
