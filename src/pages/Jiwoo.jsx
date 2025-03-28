389e8518db4e689a8ff3bd6bde500ab74eb1b459
Jiwoo <erase0250@gmail.com>
2025-03-28 10:43:45 +0900

    test: 스택 뱃지 테스트 페이지

--- src/pages/Jiwoo.jsx
+++ src/pages/Jiwoo.jsx
@@ -1,1 +1,13 @@
-// 지우 브랜치 테스트
+import StackBadge from "@/common/StackBadge";
+
+const Jiwoo = () => {
+  return (
+    <div className="flex gap-2">
+      <StackBadge text="JavaScript"/>
+      {/* 수정용 */}
+      <StackBadge text="TypeScript" showCloseIcon={true} />
+    </div>
+  );
+};
+
+export default Jiwoo;
