4d7a9e92b69e636bcc59803b098836e050f8677d
Jungseok Shin <jsjb7164@naver.com>
2025-03-28 11:45:39 +0900

    기술 스택 모달 ( 뱃지 제외한 컴포넌트 완료)

---
+++ src/pages/Jungseok.jsx
@@ -0,0 +1,12 @@
+import Layout from '../common/Layout/Layout';
+import StackModal from '@/components/common/stackModal';
+
+const Jungseok = () => {
+  return (
+    <Layout>
+      <StackModal />
+    </Layout>
+  );
+};
+
+export default Jungseok;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,15 +1,11 @@
-import Layout from "../common/Layout/Layout";
-import { Button } from "@/components/ui/button";
+import Layout from '../common/Layout/Layout';
+import { Button } from '@/components/ui/button';
 
 const Main = () => {
   return (
     <Layout>
-      <div className="text-center w-full h-60 bg-gray-700">
-        메인 페이지 입니다
-      </div>
-      <div className="text-center w-full h-60 bg-gray-700">
-        메인 페이지 입니다2
-      </div>
+      <div className="bg-sec text-center w-full h-60 bg-gray-700">메인 페이지 입니다</div>
+      <div className="text-center w-full h-60 bg-gray-700">메인 페이지 입니다2</div>
       <Button className="w-96 ">성공!</Button>
     </Layout>
   );
