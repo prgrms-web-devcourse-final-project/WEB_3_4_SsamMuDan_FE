123e63e5d8fa8bff6b16e3918175ac5c685bee95
Jungseok Shin <jsjb7164@naver.com>
2025-03-29 00:08:54 +0900

    design #33 : 카테고리 슬라이더 컴포넌트 작업

--- src/pages/Jungseok.jsx
+++ src/pages/Jungseok.jsx
@@ -7,6 +7,7 @@ import IntroduceTextArea from '@/components/common/IntroduceTextArea';
 import AddButton from '@/components/common/AddButton';
 import IntroduceBar from '@/components/common/IntroduceBar';
 import FloatingButton from '@/components/common/FloatingButton';
+import CategorySlider from '@/components/common/CategorySlider';
 
 const Jungseok = () => {
   const techStack = [
@@ -41,6 +42,9 @@ const Jungseok = () => {
       </div>
       <div>
         <FloatingButton />
+      </div>
+      <div>
+        <CategorySlider />
       </div>
     </>
   );
