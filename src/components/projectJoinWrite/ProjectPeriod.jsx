e19650ca0d83ce4cc19fee6abb6190b78f08c1b2
Jungseok Shin <jsjb7164@naver.com>
2025-04-02 16:47:52 +0900

    design #89 : 프로젝트 작성 페이지 스타일 수정

--- src/components/projectJoinWrite/ProjectPeriod.jsx
+++ src/components/projectJoinWrite/ProjectPeriod.jsx
@@ -3,7 +3,7 @@ import { Input } from '@/components/ui/input';
 const ProjectPeriod = () => {
   return (
     <>
-      <div className="w-[289px] flex flex-row gap-1 items-center">
+      <div className="w-[289px] ml-4 flex flex-row gap-1 items-center">
         {/* 시작 일자 */}
         <div className="flex flex-row items-center">
           {/* 연도 */}
@@ -11,7 +11,7 @@ const ProjectPeriod = () => {
             maxLength={4}
             type="text"
             placeholder="YYYY"
-            className="focus-visible:ring-0 w-[55px] h-[60px] ml-2 placeholder:text-[20px] text-[20px] text-grey400 border-none shadow-none px-0 "
+            className="focus-visible:ring-0 w-[45px] h-[60px] ml-2 placeholder:text-[16px] text-[16px] text-grey400 border-none shadow-none px-0 "
           />
           <span className="text-[20px] mr-2">.</span>
           {/* 월 */}
@@ -19,7 +19,7 @@ const ProjectPeriod = () => {
             maxLength={2}
             type="text"
             placeholder="MM"
-            className="focus-visible:ring-0 w-[40px] h-[60px] placeholder:text-[20px] text-[20px] text-grey400 border-none shadow-none px-0 "
+            className="focus-visible:ring-0 w-[32px] h-[60px] placeholder:text-[16px] text-[16px] text-grey400 border-none shadow-none px-0 "
             s
           />
           <span className="text-[20px] mr-2">.</span>
@@ -28,7 +28,7 @@ const ProjectPeriod = () => {
             maxLength={2}
             type="text"
             placeholder="DD"
-            className="focus-visible:ring-0 w-[40px] h-[60px] placeholder:text-[20px] text-[20px] text-grey400 border-none shadow-none px-0 "
+            className="focus-visible:ring-0 w-[32px] h-[60px] placeholder:text-[16px] text-[16px] text-grey400 border-none shadow-none px-0 "
           />
         </div>
         <div className="text-grey400">-</div>
@@ -39,7 +39,7 @@ const ProjectPeriod = () => {
             maxLength={4}
             type="text"
             placeholder="YYYY"
-            className="focus-visible:ring-0 w-[55px] h-[60px] ml-2 placeholder:text-[20px] text-[20px] text-grey400 border-none shadow-none px-0 "
+            className="focus-visible:ring-0 w-[45px] h-[60px] ml-2 placeholder:text-[16px] text-[16px] text-grey400 border-none shadow-none px-0 "
           />
           <span className="text-[20px] mr-2">.</span>
           {/* 월 */}
@@ -47,7 +47,7 @@ const ProjectPeriod = () => {
             maxLength={2}
             type="text"
             placeholder="MM"
-            className="focus-visible:ring-0 w-[40px] h-[60px] placeholder:text-[20px] text-[20px] text-grey400 border-none shadow-none px-0 "
+            className="focus-visible:ring-0 w-[32px] h-[60px] placeholder:text-[16px] text-[16px] text-grey400 border-none shadow-none px-0 "
             s
           />
           <span className="text-[20px] mr-2">.</span>
@@ -56,7 +56,7 @@ const ProjectPeriod = () => {
             maxLength={2}
             type="text"
             placeholder="DD"
-            className="focus-visible:ring-0 w-[40px] h-[60px] placeholder:text-[20px] text-[20px] text-grey400 border-none shadow-none px-0 "
+            className="focus-visible:ring-0 w-[32px] h-[60px] placeholder:text-[16px] text-[16px] text-grey400 border-none shadow-none px-0 "
           />
         </div>
       </div>
