4c46997b5469636269951a8fbb9124b51dfe51f6
Jiwoo <erase0250@gmail.com>
2025-04-02 15:51:06 +0900

    design #78 : 404 페이지 디자인 수정

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,25 +1,31 @@
 import Layout from '@/common/Layout/Layout';
-import PrimaryButton from '@/components/common/PrimaryButton';
 import { useNavigate } from 'react-router-dom';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import React from 'react';
 
 const PageNotFound = () => {
   const navigate = useNavigate();
 
   return (
     <Layout>
-      <div className="flex flex-col items-center justify-center py-16">
-        <img
-          src="/images/page-not-found.svg"
-          alt="404 페이지"
-          className="w-[500px] h-[300px] object-contain mb-10"
+      <div className="flex flex-col items-center justify-center my-[150px]">
+        <DotLottieReact
+          src="https://lottie.host/6feb3f24-ef3f-4fc5-902a-23c66c0a1b02/Xypc1483mZ.lottie"
+          loop
+          autoplay
+          className="w-[500px] h-[300px]"
         />
 
-        <p className="font-esamanru text-3xl text-grey600 mb-20">
+        <p className="font-esamanru text-3xl text-grey600 mb-12">
           요청하신 페이지를 찾을 수 없습니다.
         </p>
         <button
           onClick={() => navigate('/')}
-          className="w-[380px] h-[57px] px-6 py-3 bg-primary300 text-white font-esamanru text-lg rounded-[15px] hover:bg-primary400 transition-colors"
+          className="w-[300px] h-[50px] px-6 py-3 bg-white text-primary300 font-esamanru text-lg 
+             rounded-[999px] border border-primary300
+             hover:bg-primary300 hover:text-white
+             hover:scale-105 hover:-translate-y-1 hover:shadow-lg
+             transition-all duration-200"
         >
           메인으로 이동
         </button>
