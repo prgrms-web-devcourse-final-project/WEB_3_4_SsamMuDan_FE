599f665cb9a9227e9236531886c0744af605060e
Jungseok Shin <jsjb7164@naver.com>
2025-04-02 15:14:41 +0900

    design #82: 채용 상세페이지 스타일 수정

--- src/components/careerDetail/IntroduceSection.jsx
+++ src/components/careerDetail/IntroduceSection.jsx
@@ -1,7 +1,7 @@
 const IntroduceSection = () => {
   return (
     <>
-      <div className=" w-[1136px] h-[489px] mx-auto mt-[107px] flex flex-col gap-4">
+      <div className=" w-[1136px] h-[489px] mx-auto mt-[80px] flex flex-col gap-4">
         {/* 사진, 이름 영역 */}
         <div className="h-[217px] flex flex-row gap-7">
           <div className="w-[217px] h-[217px] rounded-[15px]">
@@ -31,7 +31,7 @@ const IntroduceSection = () => {
           </div>
         </div>
         {/* 소개글 영역 */}
-        <div className="w-full whitespace-pre-line ">
+        <div className="w-full whitespace-pre-line mt-[20px]">
           {`   Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime facilis, voluptas
               debitis ipsa esse voluptatem, incidunt rerum optio ipsum culpa rem repellendus quo!
 
