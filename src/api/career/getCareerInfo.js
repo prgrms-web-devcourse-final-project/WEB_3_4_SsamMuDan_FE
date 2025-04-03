2af73e5ecce0c2f0016a9e204f992381977ec355
Jiwoo <erase0250@gmail.com>
2025-04-03 15:22:31 +0900

    chore #99 : import 경로 수정

--- src/api/career/getCareerInfo.js
+++ src/api/career/getCareerInfo.js
@@ -1,9 +1,9 @@
-import coTreeAPI from '@/config/coTree';
+import coTreeAPI from '@/config/cotree';
 import { COTREE_ENDPOINT } from '../endpoint';
 
 const getCareerInfo = async () => {
   try {
-    const response = await coTreeAPI.get(COTREE_ENDPOINT.techbook);
+    const response = await coTreeAPI.get(COTREE_ENDPOINT.techSkills);
     console.log(response.data);
     // return response.data;
   } catch (error) {
