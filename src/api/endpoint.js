d84c2aafbc6fd17390d1ee767c21739885fd3eb4
Jiwoo <erase0250@gmail.com>
2025-04-03 15:20:38 +0900

    feat #99 : 커뮤니티 게시글 조회 엔드포인트 추가

--- src/api/endpoint.js
+++ src/api/endpoint.js
@@ -1,5 +1,7 @@
 export const COTREE_ENDPOINT = Object.freeze({
   techSkills: `/api/v1/category/skill`, // 기술 스택 조회
   techbook: `/api/v1/education/techbook`,
+  communityPosts: `/api/v1/community/board`, // 커뮤니티 게시글 조회
+
   //  예시 readQuantityAnalysis: `/readQt?authKey=${LIBRARY_KEY}`,
 });
