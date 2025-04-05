f1dd5c81147496e35406d70c11f4093132a0a9ee
Jiwoo <erase0250@gmail.com>
2025-04-05 17:51:52 +0900

    design #110 : 강의 카드 border 제거

--- src/components/main/TechBookBest.jsx
+++ src/components/main/TechBookBest.jsx
@@ -34,7 +34,7 @@ const TechBookBest = () => {
             <img
               src={item.techBookThumbnailUrl}
               alt="강의 카드 이미지"
-              className="w-full h-full object-cover border" // border는 나중에 이미지 보이면 제거
+              className="w-full h-full object-cover rounded-[10px] mt-1"
             />
           </div>
           <div className="flex flex-col ">
