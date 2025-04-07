7a5057b1a65cb7b98ba9b95652eb823051f6af0a
HO-NNE <108172790+HO-NNE@users.noreply.github.com>
2025-04-07 10:08:39 +0900

    fix#103:교육페이지 조회수정

--- src/components/common/CustomPagination.jsx
+++ src/components/common/CustomPagination.jsx
@@ -10,7 +10,7 @@ import { useEffect, useState } from 'react';
 
 const CustomPagination = ({
   totalItems,
-  itemsPerPage = 16,
+  itemsPerPage,
   pagesPerGroup = 4,
   currentPage = 1,
   onChangePage,
@@ -18,7 +18,6 @@ const CustomPagination = ({
 }) => {
   const totalPages = Math.ceil(totalItems / itemsPerPage);
   const [groupStart, setGroupStart] = useState(1);
-  // const [currentPage, setCurrentPage] = useState('0');
 
   useEffect(() => {
     const group = Math.floor((currentPage - 1) / pagesPerGroup);
@@ -57,47 +56,6 @@ const CustomPagination = ({
     return pages;
   };
   return (
-    // <Pagination className={style}>
-    //   <PaginationContent>
-    //     {/* 화살표 아이콘 */}
-    //     <PaginationItem className="hover:cursor-pointer">
-    //       <ChevronLeftIcon className="w-[25px] text-grey300" />
-    //     </PaginationItem>
-
-    //     <PaginationItem className="">
-    //       <div className="flex flex-row gap-3">
-    //         <PaginationLink
-    //           className={`w-[45px] h-[45px] border border-[#D6D6D6] ${currentPage === '1' ? 'bg-primary300 text-white' : 'text-[#D6D6D6]]'}  font-extrabold`}
-    //           href="#"
-    //         >
-    //           1
-    //         </PaginationLink>
-    //         <PaginationLink
-    //           className={`w-[45px] h-[45px] border border-[#D6D6D6] ${currentPage === '2' ? 'bg-primary300 text-white' : 'text-[#D6D6D6]'}  font-extrabold`}
-    //           href="#"
-    //         >
-    //           2
-    //         </PaginationLink>
-    //         <PaginationLink
-    //           className={`w-[45px] h-[45px] border border-[#D6D6D6] ${currentPage === '3' ? 'bg-primary300 text-white' : 'text-[#D6D6D6]'}  font-extrabold`}
-    //           href="#"
-    //         >
-    //           3
-    //         </PaginationLink>
-    //         <PaginationLink
-    //           className={`w-[45px] h-[45px] border border-[#D6D6D6] ${currentPage === '4' ? 'bg-primary300 text-white' : 'text-[#D6D6D6]'}  font-extrabold`}
-    //           href="#"
-    //         >
-    //           4
-    //         </PaginationLink>
-    //       </div>
-    //     </PaginationItem>
-    //     {/*  */}
-    //     <PaginationItem className="hover:cursor-pointer">
-    //       <ChevronRightIcon className="w-[25px] text-grey300" href="#" />
-    //     </PaginationItem>
-    //   </PaginationContent>
-    // </Pagination>
     <Pagination className={style}>
       <PaginationContent>
         <PaginationItem className="cursor-pointer" onClick={handlePrevGroup}>
