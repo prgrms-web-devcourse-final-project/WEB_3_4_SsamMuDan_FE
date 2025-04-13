5c3497a0e6b9ee136c2181093692536e6be98be5
Jiwoo <erase0250@gmail.com>
2025-04-13 12:43:46 +0900

    feat : 커뮤니티 조회페이지 이미지 없을 때 보여주는 기본 이미지 추가

--- src/api/community/postCreateCommunity.js
+++ src/api/community/postCreateCommunity.js
@@ -5,6 +5,7 @@ const postCreateCommunity = async (request) => {
   try {
     const response = await coTreeAPI.post(COTREE_ENDPOINT.communityCreate, request);
     console.log(response.data);
+    return response.data;
   } catch (error) {
     console.error('Error fetching community info:', error);
     throw error;
